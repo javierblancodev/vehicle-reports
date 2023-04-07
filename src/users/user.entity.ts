4c3bad146fe5474b50029e112765f1cfc32f5200
Javier <javierblancoit@gmail.com>
2023-04-07 07:58:29 +0100

    create entity for users and connect to root connection

---
+++ src/users/user.entity.ts
@@ -0,0 +1,14 @@
+// Create an entity file where a class list all the properties that the entity have
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
